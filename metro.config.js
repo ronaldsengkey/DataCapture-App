bd8e14fae54bd2f4219212e9abb437d52e94b587
ronald sengkey <yohanes.sengkey@amartha.com>
2026-05-20 11:12:34 -0700

    fixed bug by black box

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,11 @@
 // metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+// NativeWind v2 does not expose `nativewind/metro`.
+// NativeWind setup for Expo is handled via `babel` (`nativewind/babel`),
+// so Metro config can stay as the Expo default.
+module.exports = config;
+
+
